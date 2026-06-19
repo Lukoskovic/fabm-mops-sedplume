897a1cbeb392c40f032f7bd72ec40878cf99076c
Lukas Schreiber <lschreiber@geomar.de>
2026-06-19 15:23:35 +0200

    Code clean-up and restructure of the module set-up.

--- src/sediment.F90
+++ src/sediment.F90
@@ -38,43 +38,40 @@ contains
       class (type_mops_sediment), intent(inout), target :: self
       integer,                    intent(in)            :: configunit
 
-      ! sediment sinking parameters
-      call self%get_parameter(self%sedlambda, 'sedlambda', '1/d','sediment remineralization rate', default=0.0_rk)
-      call self%get_parameter(self%sedwb, 'sedwb', 'm/d','offset for sediment sinking', default=140.0_rk)
-      call self%get_parameter(self%sedmartin, 'sedmartin', '-','exponent for Martin curve', default=0.8580_rk)
+      ! sediment parameters
+      call self%get_parameter(self%sedwb, 'sedwb', 'm/d','sediment sinking velocity', default=140.0_rk)
+      ! following sediment parameters purely taken from detritus module, which could be equivalently treated to follow a Martin curev
+      ! like detritus if assumed the existence of organic matter that can be remineralized
+      ! here as default: no remineralization
+      call self%get_parameter(self%sedlambda, 'sedlambda', '1/d','sediment remineralization rate', default=0.0_rk) ! LS inspired by the detritus module, a remineralisation is not assumed
+      call self%get_parameter(self%sedmartin, 'sedmartin', '-','exponent for Martin curve', default=0.8580_rk)      ! LS purely
       call self%get_parameter(self%sedburdige_fac, 'sedburdige_fac', '-','factor for sediment burial (see Kriest and Oschlies, 2013)', default=1.6828_rk)
       call self%get_parameter(self%sedburdige_exp, 'sedburdige_exp', '-','exponent for sediment burial (see Kriest and Oschlies, 2013)', default=0.799_rk)
+      
+      call self%get_parameter(self%input, 'input', 'kg/s','sediment source mass rate', default=1.0_rk) ! sediment input parameter in [kg/s]
+      call self%get_parameter(self%sedarea, 'sedarea', 'm2','sediment model area', default=1.0_rk) ! assumed sediment discharge area, relevant for the 1D modelling to achieve sediment concentration per volume [kg/m3]
+      call self%get_parameter(self%ACksed, 'ACksed', '1/(m*mg/l)', 'attenuation due sediment', default=0.0452_rk)
 
-      ! sediment input parameter in [kg/s] + diagnostic variable
-      call self%get_parameter(self%input, 'input', 'kg/s','sediment input source', default=1.0_rk)
-      call self%register_diagnostic_variable(self%id_finput, 'fsed', 'kg/m2/d', 'sediment input rate', prefill_value=0.0_rk)
-      ! sediment input depth mask
-      call self%register_dependency(self%id_sedi, 'sedinput', '-', 'mask of depth for sediment input source')
-
-      ! add new parameter for the model area (in which also the sediment source will be put into!)
-      call self%get_parameter(self%sedarea, 'sedarea', 'm2','sediment model area', default=1.0_rk)
-
-      ! pelagic sediment state and diagnostic variables
+      ! register state variable for pelagic + benthic
       call self%register_state_variable(self%id_sed, 'c', 'g/l', 'concentration', minimum=0.0_rk)
-
-      ! benthic sediment and diagnostic state variables
       call self%register_bottom_state_variable(self%id_sed_burial, 'sedburial', 'kg/m2', 'sediment burial', minimum=0.0_rk)
-      call self%register_diagnostic_variable(self%id_fsed_burial, 'sedburial_flux', 'kg/m2/d', 'sediment burial flux')
 
-      ! Register environmental dependencies
+      ! register diagnostic variables
+      call self%register_diagnostic_variable(self%id_finput, 'fsed', 'kg/m2/d', 'sediment input rate', prefill_value=0.0_rk)
+      call self%register_diagnostic_variable(self%id_fsed_burial, 'sedburial_flux', 'kg/m2/d', 'sediment burial flux')
+      call self%register_diagnostic_variable(self%id_ACksed, 'att_sed', '1/m', 'attenuation due to sediment')
+
+
+      ! register environmental dependencies
       call self%register_dependency(self%id_bgc_z, standard_variables%depth)
       call self%register_dependency(self%id_bgc_z_bot, standard_variables%bottom_depth)
       call self%register_dependency(self%id_bgc_dz, standard_variables%cell_thickness)
+      ! register sediment depth input maske
+      call self%register_dependency(self%id_sedi, 'sedinput', '-', 'mask of depth for sediment input source')
 
-      ! aggregate variables for the pelagic and bentic state variables (to check mass conservation)
+      ! aggregatable variables
       call self%add_to_aggregate_variable(sediment_total, self%id_sed)
       call self%add_to_aggregate_variable(sediment_total, self%id_sed_burial)
-
-      ! initialization attenuation due to sediment
-      call self%get_parameter(self%ACksed, 'ACksed', '1/(m*mg/l)', 'attenuation due sediment', default=0.0452_rk)
-
-      call self%register_diagnostic_variable(self%id_ACksed, 'att_sed', '1/m', 'attenuation due to sediment')
-
       call self%add_to_aggregate_variable(standard_variables%attenuation_coefficient_of_photosynthetic_radiative_flux, self%id_sed, scale_factor=self%ACksed*1000.0_rk)
 
       self%dt = 86400.0_rk
@@ -90,23 +87,19 @@ contains
     _LOOP_BEGIN_
          _GET_(self%id_sed, SED)
 
-         ! just storing the attenuation due to sediment for diagnostic purposes only ! PLUME
-        att_sed = self%ACksed*1000.0_rk*SED       ! PLUME -> reason for the factor 1000: attenuation coefficient related to sediment concentration in [mg/l], needs to be used for [g/l]
-        _SET_DIAGNOSTIC_(self%id_ACksed, att_sed) ! PLUME     
+        att_sed = self%ACksed*1000.0_rk*SED       ! unit conversion (factor 1000): [kg/m3] = [g/l] -> [mg/l], often related for attenuation coefficient
+        _SET_DIAGNOSTIC_(self%id_ACksed, att_sed) 
 
         _GET_(self%id_bgc_dz, bgc_dz) ! [m]
         _GET_(self%id_sedi, idepth)
 
         ! transformation of the input from [kg/s] to [g/l/d](assuming 1m wide boxes)
-        amount_rate = self%input*idepth*self%dt  ! [kg/s] * [1] * [s/d]  = [kg/d]
-      !   amount = amount_rate/( 1.0_rk*bgc_dz )   ! [kg/d] * [m-3] = [kg/m3/d]
+        amount_rate = self%input*idepth*self%dt          ! conversion dicharge mass rate to daily rates -> [kg/s] * [1] * [s/d]  = [kg/d] 
 
-      ! now implement an area of the model, where we want to look at the experiment in an 
-        amount = amount_rate/( self%sedarea*bgc_dz )   ! [kg/d] * [m-3] = [kg/m3/d]
+        amount = amount_rate/( self%sedarea*bgc_dz )   ! sediment concentration within assumed discharged area -> [kg/d] * [m-3] = [kg/m3/d]
 
-      !   _SET_DIAGNOSTIC_(self%id_finput, amount_rate)                ! [kg/m2/d] old, unit doesn't make sense
-        _SET_DIAGNOSTIC_(self%id_finput, amount_rate)                ! [kg/d]
-        _ADD_SOURCE_(self%id_sed, amount)                            ! [kg/m3/d]
+        _SET_DIAGNOSTIC_(self%id_finput, amount_rate)                ! discharge mass rate as diagnostic [kg/d]
+        _ADD_SOURCE_(self%id_sed, amount)                            ! sediemnt concentration [kg/m3/d]
       
     
     _LOOP_END_
@@ -119,10 +112,10 @@ contains
 
       real(rk) :: sedwa, bgc_z, wsed
 
-      sedwa = self%sedlambda/self%sedmartin
+      sedwa = self%sedlambda/self%sedmartin     ! theoretical martin curve exponent: not of relevance here (assumtion of no remineralization)
       _LOOP_BEGIN_
          _GET_(self%id_bgc_z, bgc_z)
-         wsed = self%sedwb + bgc_z*sedwa
+         wsed = self%sedwb + bgc_z*sedwa        ! sediment sinking velocity, here only linear term without any further depth dependencies (no remineralisation)
          _ADD_VERTICAL_VELOCITY_(self%id_sed, -wsed)
       _LOOP_END_
    end subroutine get_vertical_movement
@@ -133,12 +126,13 @@ contains
 
       real(rk) :: sedwa, bgc_z, SED, wsed, fSED, sedflux_l
 
-      sedwa = self%sedlambda/self%sedmartin     ! [1/d]
+      ! LS 'benthic' module as implemented in same fashion like detirus
+      sedwa = self%sedlambda/self%sedmartin
       _BOTTOM_LOOP_BEGIN_
-         _GET_BOTTOM_(self%id_bgc_z_bot, bgc_z) ! [m]
-         _GET_(self%id_sed, SED)                ! [g/l] or [kg/m3]
-         wsed = self%sedwb + bgc_z*sedwa        ! [m/d]
-         fSED = wsed*SED                        ! [m/d] * [kg/m3] = [kg/m2/d]
+         _GET_BOTTOM_(self%id_bgc_z_bot, bgc_z)                            ! [m]
+         _GET_(self%id_sed, SED)                                           ! [g/l] or [kg/m3]
+         wsed = self%sedwb + bgc_z*sedwa                                   ! sediment sinking velocity [m/d]
+         fSED = wsed*SED                                                   ! [m/d] * [kg/m3] = [kg/m2/d]
          sedflux_l = MIN(1.0_rk,self%sedburdige_fac*fSED**self%sedburdige_exp)*fSED    ! [kg/m2/d]
          _ADD_BOTTOM_FLUX_(self%id_sed, -sedflux_l)
          _SET_BOTTOM_DIAGNOSTIC_(self%id_fsed_burial, sedflux_l)
